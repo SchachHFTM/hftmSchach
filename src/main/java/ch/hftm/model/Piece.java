cd8cede0f636fd8a9159cd64f5479d6f6b02e875
sebastianberchtold-hftm <sebastian.berchtold@hftm.ch>
2024-01-28 11:47:54 +0100

    getter und setter gemacht

--- src/main/java/ch/hftm/model/Piece.java
+++ src/main/java/ch/hftm/model/Piece.java
@@ -10,4 +10,30 @@ public abstract class Piece {
         this.x = x;
         this.y = y;
      }
+
+     public Color getColor(){
+        return color;
+     }
+
+     public void setColor(Color color){
+        this.color = color;
+     }
+
+     public int getX(){
+        return x;
+     }
+
+     public void setX(int x){
+        this.x = x;
+     }
+
+     public int getY(){
+        return y;
+     }
+
+     public void setY(int y){
+        this.y = y;
+     }
+
+
 }
